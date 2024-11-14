d64dd077e27cd041bae54b533bd04be165d0c631
Newton Perazzo <50239927+NewtonPerazzo@users.noreply.github.com>
2024-11-14 16:32:38 -0300

    feature(add-project-structure): create main layout and header

---
+++ src/components/Header/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: #007bff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  box-sizing: border-box;
+`;
+
+export { Container };
